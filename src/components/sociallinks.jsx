3b90df28b4d1f1b6f2a50a0c73b2f5137e12345a
Ashish B <cameron.ashish@gmail.com>
2023-10-02 23:56:54 +0530

    commit 2/10

--- src/components/sociallinks.jsx
+++ src/components/sociallinks.jsx
@@ -13,7 +13,7 @@ const Sociallinks = () => {
 
       <a href="https://wa.me/+917898691929" target="blank">
         <img
-          className="w-10 h-10 mx-1 transition-all duration-700 ease-in-out delay-75 rounded-full hover:bg-white hover:scale-110 hover:rotate-[360deg]"
+          className="w-10 h-10 mx-1 transition-all duration-1000 ease-in-out delay-75 rounded-full hover:bg-white hover:scale-110 hover:rotate-[360deg]"
           src="https://cdn4.iconfinder.com/data/icons/social-media-free-13/32/whatsapp_social_media_logo--256.png"
           alt="whatsapp"
         />
@@ -24,7 +24,7 @@ const Sociallinks = () => {
         target="blank"
       >
         <img
-          className="w-10 h-10 mx-1 transition-all duration-700 ease-in-out delay-75 rounded-full hover:bg-white hover:scale-110 hover:rotate-[360deg]"
+          className="w-10 h-10 mx-1 transition-all duration-1000 ease-in-out delay-75 rounded-full hover:bg-white hover:scale-110 hover:rotate-[360deg]"
           src="https://cdn1.iconfinder.com/data/icons/capsocial/500/linkedin2-256.png"
           alt="linkedin"
         />
@@ -32,7 +32,7 @@ const Sociallinks = () => {
 
       <a href="https://www.instagram.com/_._.ashish/" target="blank">
         <img
-          className="w-10 h-10 mx-1 transition-all duration-700 ease-in-out delay-75 rounded-full hover:bg-white hover:scale-110 hover:rotate-[360deg] "
+          className="w-10 h-10 mx-1 transition-all duration-1000 ease-in-out delay-75 rounded-full hover:bg-white hover:scale-110 hover:rotate-[360deg] "
           src="https://cdn1.iconfinder.com/data/icons/social-media-circle-7/512/Circled_Instagram_svg-256.png"
           alt="instagram"
         />
@@ -40,7 +40,7 @@ const Sociallinks = () => {
 
       <a href="https://github.com/ashish1seven" target="blank">
         <img
-          className="w-10 h-10 ml-1 transition-all duration-700 ease-in-out delay-75 rounded-full hover:bg-white hover:scale-110 hover:rotate-[360deg]"
+          className="w-10 h-10 ml-1 transition-all duration-1000 ease-in-out delay-75 rounded-full hover:bg-white hover:scale-110 hover:rotate-[360deg]"
           src="https://cdn3.iconfinder.com/data/icons/font-awesome-brands/512/github-256.png"
           alt="github"
         />
